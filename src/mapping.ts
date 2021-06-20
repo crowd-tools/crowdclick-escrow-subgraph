e84005ce14bbcf8e4a57c5feccf0e55da5adeff7
RiccardoBiosas <rbios@protonmail.com>
2021-06-20 17:44:01 +0200

    refactored publisher entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,61 +7,23 @@ import {
   RewardForwarded,
   UserWithdrawalEmitted
 } from "../generated/CrowdclickEscrow/CrowdclickEscrow"
-import { ExampleEntity } from "../generated/schema"
+import { Publisher } from "../generated/schema"
 
 export function handleCampaignCreated(event: CampaignCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Publisher.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
+    entity = new Publisher(event.transaction.from.toHex())
     entity.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
   entity.count = entity.count + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
-  entity.publisher = event.params.publisher
+  entity.address = event.params.publisher
   entity.campaignBudget = event.params.campaignBudget
+  entity.campaignUrl = event.params.campaignUrl
 
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.collectedFee(...)
-  // - contract.divider(...)
-  // - contract.feeCollector(...)
-  // - contract.feePercentage(...)
-  // - contract.lastUserWithdrawalTime(...)
-  // - contract.maximumWeiUserWithdrawal(...)
-  // - contract.minimumUsdWithdrawal(...)
-  // - contract.multiplier(...)
-  // - contract.owner(...)
-  // - contract.balanceOfPublisher(...)
-  // - contract.balanceOfUser(...)
-  // - contract.isUserWithdrawalEnabled(...)
-  // - contract.lookupTask(...)
-  // - contract.calculateWithdrawalRate(...)
 }
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
