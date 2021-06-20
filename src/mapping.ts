32165024ba02cfced3202ad796baf743a33fbe8d
RiccardoBiosas <rbios@protonmail.com>
2021-06-20 19:13:26 +0200

    added mapping for user entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -7,7 +7,7 @@ import {
   RewardForwarded,
   UserWithdrawalEmitted
 } from "../generated/CrowdclickEscrow/CrowdclickEscrow"
-import { Publisher } from "../generated/schema"
+import { Publisher, User } from "../generated/schema"
 
 export function handleCampaignCreated(event: CampaignCreated): void {
   let entity = Publisher.load(event.transaction.from.toHex())
@@ -32,7 +32,22 @@ export function handlePublisherWithdrawalEmitted(
   event: PublisherWithdrawalEmitted
 ): void {}
 
-export function handleRewardForwarded(event: RewardForwarded): void {}
+export function handleRewardForwarded(event: RewardForwarded): void {
+  let entity = User.load(event.params.recipient.toHex())
+
+  if (entity == null) {
+    entity = new User(event.params.recipient.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+
+  entity.address = event.params.recipient
+  entity.reward = event.params.reward
+  entity.campaignUrl = event.params.campaignUrl
+
+  entity.save()
+}
 
 export function handleUserWithdrawalEmitted(
   event: UserWithdrawalEmitted
